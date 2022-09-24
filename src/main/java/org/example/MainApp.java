abf67d149deeb43e25e65d26b6569ea338bc41f0
NoahSXF <994518231@qq.com>
2022-09-24 22:33:59 +0800

    学习gradle的基本用法，集成springboot

---
+++ src/main/java/org/example/MainApp.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 练习
+ *
+ * @BelongsProject: MainApp
+ * @BelongsPackage: org.example
+ * @Author: Noah
+ * @CreateTime: 2022-09-24  22:16
+ * @Description: 练习
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class MainApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp.class, args);
+    }
+}
